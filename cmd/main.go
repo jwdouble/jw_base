94b1f68284bcce3a4bdb7dd13487db8f6d839ae3
jwdouble <954649468@qq.com>
2022-05-20 18:32:13 +0800

    feat: grpc

--- cmd/main.go
+++ cmd/main.go
@@ -1,29 +1,70 @@
 package main
 
 import (
-	"context"
-	_ "embed"
-	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
-	"log"
-	"net"
-	"net/http"
-
-	"google.golang.org/grpc"
-
 	pb "cmd/main.go/proto/generated_go"
 	"cmd/main.go/service"
+	"context"
+	_ "embed"
+	"fmt"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"net"
+	"net/http"
+	"strings"
 )
 
 //go:embed generated_doc.swagger.json
 var swaggerJSON string
 
+const addr = "localhost:8081"
+
 func main() {
-	log.Println("jw-base start run")
-	lis, err := net.Listen("tcp", "localhost:8080")
+	fmt.Printf("Starting server on %s\n", addr)
+	grpcServer := grpc.NewServer()
+	pb.RegisterBaseServiceServer(grpcServer, service.NewBaseService())
+
+	// 创建http路由和wg路由
+	mux := http.NewServeMux()
+	mux.HandleFunc("/index", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("1234"))
+	})
+
+	gwmux := runtime.NewServeMux()
+	dopts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+	err := pb.RegisterBaseServiceHandlerFromEndpoint(context.Background(), gwmux, addr, dopts)
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		panic(err)
 	}
 
-	http.
+	mux.Handle("/", gwmux)
 
+	conn, err := net.Listen("tcp", addr)
+	if err != nil {
+		panic(err)
+	}
+
+	srv := &http.Server{
+		Addr:    addr,
+		Handler: grpcHandleFunc(grpcServer, mux),
+	}
+
+	err = srv.Serve(conn)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("exit")
+}
+
+func grpcHandleFunc(grpcServer *grpc.Server, httpHandler http.Handler) http.Handler {
+	return h2c.NewHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.ProtoMajor == 2 && strings.Contains(r.Header.Get("Content-Type"), "application/grpc") {
+			grpcServer.ServeHTTP(w, r)
+		} else {
+			httpHandler.ServeHTTP(w, r)
+		}
+	}), &http2.Server{})
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.10.0
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd
 	google.golang.org/genproto v0.0.0-20220317150908-0efb43f6373e
 	google.golang.org/grpc v1.45.0
 	google.golang.org/protobuf v1.27.1
@@ -12,7 +13,6 @@ require (
 
 require (
 	github.com/golang/protobuf v1.5.2 // indirect
-	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
 	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
 	golang.org/x/text v0.3.7 // indirect
 )
