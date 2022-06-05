35a767ffe740673562bc1e174aebcaeb2f2ed245
jwdouble <954649468@qq.com>
2022-06-05 21:00:58 +0800

    feat

--- cmd/main.go
+++ cmd/main.go
@@ -4,13 +4,16 @@ import (
 	pb "cmd/main.go/proto/generated_go"
 	"cmd/main.go/service"
 	"context"
+	"crypto/tls"
 	_ "embed"
 	"fmt"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/grpclog"
+	"io/ioutil"
 	"net"
 	"net/http"
 	_ "net/http/pprof"
@@ -24,6 +27,12 @@ const addr = ":10000"
 
 func main() {
 	fmt.Printf("Starting server on %s\n", addr)
+	//tls, err := credentials.NewServerTLSFromFile("/static/tls.pem", "/static/tls.key")
+	//if err != nil {
+	//	panic(err)
+	//}
+
+	//grpcServer := grpc.NewServer(grpc.Creds(tls))
 	grpcServer := grpc.NewServer()
 	pb.RegisterBaseServiceServer(grpcServer, service.NewBaseService())
 
@@ -34,13 +43,10 @@ func main() {
 	})
 
 	gwmux := runtime.NewServeMux()
-	tls, err := credentials.NewServerTLSFromFile("/file/tls/tls.crt", "/file/tls/tls.key")
-	if err != nil {
-		panic(err)
-	}
 
-	dopts := []grpc.DialOption{grpc.WithTransportCredentials(tls)} // insecure.NewCredentials() 默认安全模式
-	err = pb.RegisterBaseServiceHandlerFromEndpoint(context.Background(), gwmux, addr, dopts)
+	//dopts := []grpc.DialOption{grpc.WithTransportCredentials(tls)} // insecure.NewCredentials() 默认安全模式
+	dopts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())} // insecure.NewCredentials() 默认安全模式
+	err := pb.RegisterBaseServiceHandlerFromEndpoint(context.Background(), gwmux, addr, dopts)
 	if err != nil {
 		panic(err)
 	}
@@ -55,6 +61,7 @@ func main() {
 	srv := &http.Server{
 		Addr:    addr,
 		Handler: grpcHandleFunc(grpcServer, mux),
+		//TLSConfig: getTLSConfig(),
 	}
 
 	err = srv.Serve(conn)
@@ -68,10 +75,26 @@ func main() {
 // 选择合适的处理
 func grpcHandleFunc(grpcServer *grpc.Server, httpHandler http.Handler) http.Handler {
 	return h2c.NewHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		if r.ProtoMajor == 2 && strings.Contains(r.Header.Get("Content-Type"), "application/grpc") {
 			grpcServer.ServeHTTP(w, r)
 		} else {
 			httpHandler.ServeHTTP(w, r)
 		}
 	}), &http2.Server{})
+}
+
+func getTLSConfig() *tls.Config {
+	cert, _ := ioutil.ReadFile("/static/tls.pem")
+	key, _ := ioutil.ReadFile("/static/tls.key")
+	var demoKeyPair *tls.Certificate
+	pair, err := tls.X509KeyPair(cert, key)
+	if err != nil {
+		grpclog.Fatalf("TLS KeyPair err: %v\n", err)
+	}
+	demoKeyPair = &pair
+	return &tls.Config{
+		Certificates: []tls.Certificate{*demoKeyPair},
+		NextProtos:   []string{http2.NextProtoTLS}, // HTTP2 TLS支持
+	}
 }
