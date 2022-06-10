2840390227f814ae21f8d32080eb0de9dda47180
wang_ju <wang_ju@arcplus.com.cn>
2022-06-10 10:23:38 +0800

    fix

--- cmd/main.go
+++ cmd/main.go
@@ -1,39 +1,38 @@
 package main
 
 import (
-	pb "cmd/main.go/proto/generated_go"
-	"cmd/main.go/service"
 	"context"
 	"crypto/tls"
 	_ "embed"
 	"fmt"
+	"net"
+	"net/http"
+	_ "net/http/pprof"
+	"os"
+	"strings"
+
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
-	"google.golang.org/grpc/grpclog"
-	"io/ioutil"
-	"net"
-	"net/http"
-	_ "net/http/pprof"
-	"strings"
-)
+	"google.golang.org/grpc/credentials"
+	"jw.lib/logx"
 
-//go:embed generated_doc.swagger.json
-var swaggerJSON string
+	pb "cmd/main.go/proto/generated_go"
+	"cmd/main.go/service"
+)
 
 const addr = ":10000"
 
 func main() {
 	fmt.Printf("Starting server on %s\n", addr)
-	//tls, err := credentials.NewServerTLSFromFile("/static/tls.pem", "/static/tls.key")
-	//if err != nil {
-	//	panic(err)
-	//}
+	tls, err := credentials.NewServerTLSFromFile("/static/tls.pem", "/static/tls.key")
+	if err != nil {
+		panic(err)
+	}
 
-	//grpcServer := grpc.NewServer(grpc.Creds(tls))
-	grpcServer := grpc.NewServer()
+	grpcServer := grpc.NewServer(grpc.Creds(tls))
+	//grpcServer := grpc.NewServer()
 	pb.RegisterBaseServiceServer(grpcServer, service.NewBaseService())
 
 	// 创建http路由和wg路由
@@ -44,9 +43,9 @@ func main() {
 
 	gwmux := runtime.NewServeMux()
 
-	//dopts := []grpc.DialOption{grpc.WithTransportCredentials(tls)} // insecure.NewCredentials() 默认安全模式
-	dopts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())} // insecure.NewCredentials() 默认安全模式
-	err := pb.RegisterBaseServiceHandlerFromEndpoint(context.Background(), gwmux, addr, dopts)
+	dopts := []grpc.DialOption{grpc.WithTransportCredentials(tls)} // insecure.NewCredentials() 默认安全模式
+	//dopts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())} // insecure.NewCredentials() 默认安全模式
+	err = pb.RegisterBaseServiceHandlerFromEndpoint(context.Background(), gwmux, addr, dopts)
 	if err != nil {
 		panic(err)
 	}
@@ -59,9 +58,9 @@ func main() {
 	}
 
 	srv := &http.Server{
-		Addr:    addr,
-		Handler: grpcHandleFunc(grpcServer, mux),
-		//TLSConfig: getTLSConfig(),
+		Addr:      addr,
+		Handler:   grpcHandleFunc(grpcServer, mux),
+		TLSConfig: getTLSConfig(),
 	}
 
 	err = srv.Serve(conn)
@@ -75,7 +74,7 @@ func main() {
 // 选择合适的处理
 func grpcHandleFunc(grpcServer *grpc.Server, httpHandler http.Handler) http.Handler {
 	return h2c.NewHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "*") // 允许所有跨域请求
 		if r.ProtoMajor == 2 && strings.Contains(r.Header.Get("Content-Type"), "application/grpc") {
 			grpcServer.ServeHTTP(w, r)
 		} else {
@@ -85,16 +84,17 @@ func grpcHandleFunc(grpcServer *grpc.Server, httpHandler http.Handler) http.Hand
 }
 
 func getTLSConfig() *tls.Config {
-	cert, _ := ioutil.ReadFile("/static/tls.pem")
-	key, _ := ioutil.ReadFile("/static/tls.key")
-	var demoKeyPair *tls.Certificate
-	pair, err := tls.X509KeyPair(cert, key)
+	pem, _ := os.ReadFile("/static/tls.pem")
+	key, _ := os.ReadFile("/static/tls.key")
+	logx.Info(string(pem))
+	logx.Info(string(key))
+	cert, err := tls.X509KeyPair(pem, key)
 	if err != nil {
-		grpclog.Fatalf("TLS KeyPair err: %v\n", err)
+		logx.Error("tls load err: %s", err)
 	}
-	demoKeyPair = &pair
+
 	return &tls.Config{
-		Certificates: []tls.Certificate{*demoKeyPair},
+		Certificates: []tls.Certificate{cert},
 		NextProtos:   []string{http2.NextProtoTLS}, // HTTP2 TLS支持
 	}
 }
--- go.mod
+++ go.mod
@@ -5,14 +5,36 @@ go 1.18
 require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.10.0
-	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd
-	google.golang.org/genproto v0.0.0-20220317150908-0efb43f6373e
+	golang.org/x/net v0.0.0-20220412020605-290c469a71a5
+	google.golang.org/genproto v0.0.0-20220407144326-9054f6ed7bac
 	google.golang.org/grpc v1.45.0
-	google.golang.org/protobuf v1.27.1
+	google.golang.org/protobuf v1.28.0
+	jw.lib v0.0.0-00010101000000-000000000000
 )
 
 require (
+	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/go-redis/redis v6.15.9+incompatible // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
-	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/lib/pq v1.10.4 // indirect
+	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/mitchellh/mapstructure v1.4.3 // indirect
+	github.com/nxadm/tail v1.4.8 // indirect
+	github.com/pelletier/go-toml v1.9.4 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.0-beta.8 // indirect
+	github.com/rs/zerolog v1.26.1 // indirect
+	github.com/spf13/afero v1.8.2 // indirect
+	github.com/spf13/cast v1.4.1 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.11.0 // indirect
+	github.com/subosito/gotenv v1.2.0 // indirect
+	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
 	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/ini.v1 v1.66.4 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
+
+replace jw.lib => ../jw_lib
