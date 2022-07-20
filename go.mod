42f28a0695ff570f5d5f686b43ba5294daee56e1
wang_ju <wang_ju@arcplus.com.cn>
2022-07-20 15:30:36 +0800

    feat: migration

--- go.mod
+++ go.mod
@@ -4,9 +4,8 @@ go 1.18
 
 require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible
-	github.com/golang-migrate/migrate v3.5.4+incompatible
+	github.com/golang-migrate/migrate/v4 v4.15.2
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.10.0
-	github.com/lib/pq v1.10.4
 	golang.org/x/net v0.0.0-20220412020605-290c469a71a5
 	google.golang.org/genproto v0.0.0-20220407144326-9054f6ed7bac
 	google.golang.org/grpc v1.45.0
@@ -18,10 +17,12 @@ require (
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/go-redis/redis v6.15.9+incompatible // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/lib/pq v1.10.4 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
 	github.com/mitchellh/mapstructure v1.4.3 // indirect
-	github.com/nxadm/tail v1.4.8 // indirect
 	github.com/pelletier/go-toml v1.9.4 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.0-beta.8 // indirect
 	github.com/rs/zerolog v1.26.1 // indirect
@@ -31,6 +32,7 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/spf13/viper v1.11.0 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
 	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/ini.v1 v1.66.4 // indirect
--- sqlc/init.go
+++ sqlc/init.go
@@ -1,9 +1,13 @@
 package sqlc
 
 import (
-	"github.com/golang-migrate/migrate"
-	_ "github.com/golang-migrate/migrate/source/file"
-	_ "github.com/lib/pq"
+	"fmt"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"jw.lib/logx"
+	"jw.lib/sqlx"
 
 	"cmd/main.go/sqlc/gen"
 )
@@ -12,29 +16,48 @@ var Q *gen.Queries
 var M *migrate.Migrate
 
 func init() {
-	//sqlx.Register(sqlx.DefaultSqlDriver, sqlx.DefaultSqlAddr)
-	//db := sqlx.GetSqlOperator()
-	//
-	//ins, err := stub.WithInstance(db, &stub.Config{})
-	//if err != nil {
-	//	logx.Error("stub.WithInstance",err)
-	//	return
-	//}
-	//
-	//M, err = migrate.NewWithDatabaseInstance("file://sqlc/migration", "postgres", ins)
-	//if err != nil {
-	//	logx.Error("migrate.NewWithDatabaseInstance",err)
-	//	return
-	//}
-	//
-	//err = M.Up()
-	//if err != nil {
-	//	fmt.Println("err-->", err)
-	//} else {
-	//	fmt.Println("migrate success")
-	//}
+	sqlx.Register(sqlx.Driver, sqlx.PGConfigMap)
+	db := sqlx.GetSqlOperator()
+
+	// 看文档得看清楚版本和更新日期
+	// go mod默认使用v1版本，需要自己确认模块的最新版本是什么
+	di, err := postgres.WithInstance(db, &postgres.Config{MigrationsTable: "_migration"})
+	if err != nil {
+		logx.Error("stub.WithInstance", err)
+		return
+	}
+
+	M, err = migrate.NewWithDatabaseInstance("file:///home/jw/files/self/jw_base/sqlc/migration", "postgres", di)
+	if err != nil {
+		logx.Error("migrate.NewWithDatabaseInstance", err)
+		return
+	}
+
+	M.Log = &Log{}
+
+	fmt.Println(M.Version())
+
+	err = M.Up()
+	if err != nil {
+		fmt.Println("err-->", err)
+	} else {
+		fmt.Println("migrate success")
+	}
 }
 
 func Register() {
 
 }
+
+type Log struct {
+}
+
+func (l Log) Printf(format string, v ...interface{}) {
+	fmt.Printf(format+"\n", v...)
+}
+
+func (l Log) Verbose() bool {
+	return true
+}
+
+var _ migrate.Logger = (*Log)(nil)
